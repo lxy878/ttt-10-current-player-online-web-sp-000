af0f0f8232146c75455fc1157c78d3a6b661b839
Xiaoyi Liu <lxy878@gmail.com>
2020-03-02 05:15:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,7 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-
+  if turn_count(board)%2 == 0
+    return "X"
+  else
+    return "O"
+  end
 end
-
-puts turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
